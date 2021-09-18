fd6002923e22c78e496ce3056f2e9aaf5df787af
Aiden <tyjk32@gmail.com>
2021-09-18 20:18:28 +0900

    Bug fix

--- src/main/java/com/aiden/dev/simpleboard/modules/main/MainController.java
+++ src/main/java/com/aiden/dev/simpleboard/modules/main/MainController.java
@@ -6,6 +6,7 @@ import com.aiden.dev.simpleboard.modules.post.Post;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,7 +32,7 @@ public class MainController {
                        @RequestParam(value = "keyword", defaultValue = "") String keyword,
                        @RequestParam(value = "page", defaultValue = "1") int page,
                        @CurrentAccount Account account, Model model) {
-        Page<Post> posts = postService.getPosts(PageRequest.of(page - 1, 10), category, keyword);
+        Page<Post> posts = postService.getPosts(PageRequest.of(page - 1, 10, Sort.by(Sort.Direction.DESC, "createdAt")), category, keyword);
         model.addAttribute("posts", posts);
         model.addAttribute("category", category);
         model.addAttribute("keyword", keyword);
